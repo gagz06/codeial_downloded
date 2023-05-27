7d02935a84dfe7e09b0f593a2f94bb7b7c49868b
Gagan <gagan3809@gmail.com>
2023-05-27 16:24:50 +0530

    settiung up passport

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+
+const LocalStartegy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+//aurthentication using passport
+passport.use(new LocalStartegy({
+    usernameField: 'email'
+    },
+    function(email,password,done){
+        //find user and establish  the identity
+        User.findOne({email: email})
+        .then((user)=>{
+            if(!user||user.password!=password){
+                console.log('Invalid username/password');
+                return done(null,false);
+            }
+            return done(null,user);
+        })
+        .catch((err)=>{
+            console.log('Error in finding user --> passport');
+            return done(err);
+        });
+
+    }
+));
+
+//serializing the user  to decide which key is to be keptin cookies
+passport.serializeUser(function(user,done){
+done(null,user.id);
+});
+
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id,done){
+User.findById(id)
+.then(()=>{
+    return done(null,user);
+})
+.catch((err)=>{ 
+    console.log('Error in finding user --> passport');
+    return done(err);});
+});
+
+module.exports = passport;
