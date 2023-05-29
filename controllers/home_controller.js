04056c708e6103d4c6e746590f23ba4daaaadb3a
Gagan <gagan3809@gmail.com>
2023-05-29 22:59:14 +0530

    post shown on home page according to user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,41 @@
+const Post = require('../models/post')
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('id',30);
-    return res.render('home', {
-        title: "Home"
+    //console.log(req.cookies);
+    //res.cookie('id',30);
+    // Post.find({}).then((posts)=>{
+    //     return res.render('home', {
+    //         title: "Ciodeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    Post.find({}).populate('user').then((posts)=>{
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
+
+
+    // code conerted by chat gpt
+//     Post.find({})
+//   .populate('user')
+//   .then(posts => {
+//     return res.render('home', {
+//       title: "Ciodeial | Home",
+//       posts: posts
+//     });
+//   })
+//   .catch(err => {
+//     // Handle error
+//     console.error(err);
+//     // Return an error response
+//     return res.status(500).json({ error: 'Internal Server Error' });
+//   });
+
+
+
 }
 
 // module.exports.actionName = function(req, res){}
