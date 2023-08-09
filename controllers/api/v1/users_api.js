32a61a4c20edad221456a41ba631a99344b76899
Gagan Dahiya <gagan3809@gmail.com>
2023-08-09 23:34:14 +0530

    authenticate and delete post

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -13,7 +13,7 @@ module.exports.createSession = async function (req, res) {
     return res.status(200).json({
         message: 'Sign in Successful, here is your token, please keep it safe',
         data:{
-            token : jwt.sign(user.toJSON(),'codeial',{expiresIn: '10000'})
+            token : jwt.sign(user.toJSON(),'codeial',{expiresIn: '100000'})
         }
     })
   } catch (err) {
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const postsApi = require("../../../controllers/api/v1/posts_api");
 
 router.get('/',postsApi.index);
-
-router.delete('/:id',postsApi.destroy);
+// jwt is strategy and session false to avoid generating session cookies for api
+router.delete('/:id',passport.authenticate('jwt',{session: false}),postsApi.destroy);
 
 
 module.exports = router;
